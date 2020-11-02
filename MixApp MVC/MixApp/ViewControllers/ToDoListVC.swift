9197e23b48b7c3bd40e879236df624a5abdc7984
Talgar <talgarrmelisbekov@gmail.com>
2020-11-02 23:58:55 +0600

    start app

---
+++ MixApp MVC/MixApp/ViewControllers/ToDoListVC.swift
@@ -0,0 +1,178 @@
+//
+//  TableViewController.swift
+//  ListOfPlan
+//
+//  Created by admin on 20.06.2020.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListVC: UITableViewController {
+    
+    @IBOutlet weak var menuBtn: UIBarButtonItem!
+    
+    var filteredToDo = [Items]()
+    var item : Items? = nil
+    
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Items"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        tableView.reloadData()
+        loadData()
+        sideMenu()
+    }
+    
+    
+    // MARK: - SideMenuFunc
+    
+    func sideMenu() {
+        if revealViewController() != nil {
+            
+            menuBtn.target = revealViewController()
+            menuBtn.action = #selector(SWRevealViewController.revealToggle(_:))
+            revealViewController()?.rearViewRevealWidth = 300
+            
+            view.addGestureRecognizer((self.revealViewController()?.panGestureRecognizer())!)
+        }
+    }
+    
+    //MARK: - SearchBar
+    
+    func isSearchBarEmpty() -> Bool {
+      return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String) {
+        filteredToDo = toDoItems.filter({( task : Items) -> Bool in
+            return task.name!.lowercased().contains(searchText.lowercased())
+        })
+        tableView.reloadData()
+    }
+    
+    func isFiltering() -> Bool {
+        return searchController.isActive && !isSearchBarEmpty()
+    }
+    
+    
+    @IBAction func editAct(_ sender: Any) {
+        
+        tableView.setEditing(!tableView.isEditing, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            self.tableView.reloadData()
+            saveData()
+        }
+    }
+    
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if isFiltering() {
+        return filteredToDo.count
+      }
+        return toDoItems.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        if isFiltering() {
+            item = filteredToDo[indexPath.row]
+        } else {
+            item = toDoItems[indexPath.row]
+        }
+        
+        cell.textLabel?.text = (item?.name)
+        
+        if (item!.completed) == true  {
+            cell.imageView?.image = #imageLiteral(resourceName: "check")
+        } else {
+           cell.imageView?.image = #imageLiteral(resourceName: "uncheck")
+        }
+
+        if tableView.isEditing {
+            cell.textLabel?.alpha = 0.4
+            cell.imageView?.alpha = 0.4
+        } else {
+            cell.textLabel?.alpha = 1
+            cell.imageView?.alpha = 1
+        }
+        
+        return cell
+    }
+
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            
+            removeItem(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if changeState(at: indexPath.row) {
+            tableView.cellForRow(at: indexPath)?.imageView?.image = #imageLiteral(resourceName: "check")
+        } else {
+            tableView.cellForRow(at: indexPath)?.imageView?.image = #imageLiteral(resourceName: "uncheck")
+        }
+        saveData()
+    }
+
+    
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        
+        moveItem(fromIndex: fromIndexPath.row, toIndex: to.row)
+        tableView.reloadData()
+        saveData()
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        
+        if tableView.isEditing {
+            return .none
+        } else {
+             return .delete
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+}
+
+extension ToDoListVC : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
+}
+
